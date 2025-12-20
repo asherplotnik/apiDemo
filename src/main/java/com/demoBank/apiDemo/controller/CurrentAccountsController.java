a2813ae97ea59fff65a969f78b69caa35bfbaf84
asherplotnik <aserplotnik@gmail.com>
2025-12-20 13:35:56 +0200

    enhance filter current accounts

--- src/main/java/com/demoBank/apiDemo/controller/CurrentAccountsController.java
+++ src/main/java/com/demoBank/apiDemo/controller/CurrentAccountsController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/current-accounts")
@@ -25,9 +26,11 @@ public class CurrentAccountsController {
             @RequestParam(required = false, defaultValue = "true") Boolean includePending,
             @RequestParam(required = false, defaultValue = "100") Integer pageSize,
             @RequestParam(required = false) String cursor,
-            @RequestParam(required = false, defaultValue = "Asia/Jerusalem") String timezone) {
+            @RequestParam(required = false, defaultValue = "Asia/Jerusalem") String timezone,
+            @RequestParam(required = false) List<String> accountList,
+            @RequestParam(required = false, defaultValue = "true") Boolean includeTransactions) {
         try {
-            Document data = currentAccountsService.getCurrentAccountsData(customerID);
+            Document data = currentAccountsService.getCurrentAccountsData(customerID, fromDate, toDate, accountList, includeTransactions);
             return ResponseEntity.ok(data);
         } catch (IOException e) {
             return ResponseEntity.internalServerError().build();
